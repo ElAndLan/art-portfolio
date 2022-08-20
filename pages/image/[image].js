207b1d3b3e68e55a535ddb0fff53800c34cdad56
Andrew LaNoue <alanoue@catalyte.io>
2022-08-20 16:12:08 -0400

    Worked on styling

--- pages/image/[image].js
+++ pages/image/[image].js
@@ -1,10 +1,11 @@
 import { useRouter } from 'next/router';
+import Image from "next/image";
 import Data from "../../data";
 import styles from "../../styles/ImageView.module.css";
 import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/footer";
 
-const Image = () => {
+const ImagePage = () => {
     const router = useRouter();
     const pid = router.query.image;
     const location = pid - 1;
@@ -13,7 +14,7 @@ const Image = () => {
         <div className={styles.container}>
         <Header />
             <div className={styles.imageContainer}>
-                <img className={styles.image} src={Data[location].url}></img>
+                <Image className={styles.image} src={Data[location].url}></Image>
             </div>
             <div className={styles.infoContainer}>
                 The info of the picture will go here. So Date, Medium(Pencil&Paper, Ink,Digital) as well as a short
@@ -24,4 +25,4 @@ const Image = () => {
     )
 }
 
-export default Image;
+export default ImagePage;
